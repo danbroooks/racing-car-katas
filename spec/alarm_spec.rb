7f8898ec738331ee0e8c401d18002ef8491c7ebf
Dan Brooks <dmbrooks@live.co.uk>
2017-10-09 14:33:23 +0100

    Add tests for when pressure is greater than threshold as well than less than

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe Alarm do
     expect(subject.is_alarm_on).to be(false)
   end
 
-  it 'should turn on when pressure is not within threshold range' do
+  it 'should not trigger the alarm when the pressure is in the threshold range' do
+    allow(Sensor).to receive(:sample_pressure) { 5 }
+    subject.check
+    expect(subject.is_alarm_on).to be(false)
+  end
+
+  it 'should turn on when pressure is less than threshold range' do
     allow(Sensor).to receive(:sample_pressure) { 0 }
     subject.check
     expect(subject.is_alarm_on).to be(true)
   end
 
-  it 'should not trigger the alarm when the pressure is in the threshold range' do
-    allow(Sensor).to receive(:sample_pressure) { 5 }
+  it 'should turn on when pressure is greater than threshold range' do
+    allow(Sensor).to receive(:sample_pressure) { 10 }
     subject.check
-    expect(subject.is_alarm_on).to be(false)
+    expect(subject.is_alarm_on).to be(true)
   end
 end
