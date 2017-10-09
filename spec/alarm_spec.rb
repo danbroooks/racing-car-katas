ab41c038a3e7f273b54172bceb2358968fd6897f
Dan Brooks <dmbrooks@live.co.uk>
2017-10-09 14:32:00 +0100

    Test that alarm is not triggered when pressure is fine

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe Alarm do
     expect(subject.is_alarm_on).to be(true)
   end
 
+  it 'should not trigger the alarm when the pressure is in the threshold range' do
+    allow(Sensor).to receive(:sample_pressure) { 5 }
+    subject.check
+    expect(subject.is_alarm_on).to be(false)
+  end
 end
