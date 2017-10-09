742463ffebce8ffccfe2e8286c6bf377e8aafff1
Dan Brooks <dmbrooks@live.co.uk>
2017-10-09 15:05:48 +0100

    Update api for alarm

--- lib/alarm.rb
+++ lib/alarm.rb
@@ -2,18 +2,13 @@ require 'sensor'
 
 class Alarm
 
-  attr_reader :is_alarm_on
-
   def initialize(sensor)
     @threshold = (17..21)
     @sensor = sensor
-    @is_alarm_on = false
   end
 
-  def check
-    unless @threshold.include?(psi_pressure_value)
-      @is_alarm_on = true
-    end
+  def on?
+    @threshold.include?(psi_pressure_value) == false
   end
 
   private
--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -5,25 +5,18 @@ RSpec.describe Alarm do
     Alarm.new(Sensor.new())
   }
 
-  it 'should be turned off by default' do
-    expect(subject.is_alarm_on).to be(false)
-  end
-
   it 'should not trigger the alarm when the pressure is in the threshold range' do
     allow(Sensor).to receive(:sample_pressure) { 5 }
-    subject.check
-    expect(subject.is_alarm_on).to be(false)
+    expect(subject).not_to be_on
   end
 
   it 'should turn on when pressure is less than threshold range' do
     allow(Sensor).to receive(:sample_pressure) { 0 }
-    subject.check
-    expect(subject.is_alarm_on).to be(true)
+    expect(subject).to be_on
   end
 
   it 'should turn on when pressure is greater than threshold range' do
     allow(Sensor).to receive(:sample_pressure) { 10 }
-    subject.check
-    expect(subject.is_alarm_on).to be(true)
+    expect(subject).to be_on
   end
 end
