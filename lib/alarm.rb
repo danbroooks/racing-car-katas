c8a9f35346b440b5b1e6a623ed0d24bb9cfaad8e
Dan Brooks <dmbrooks@live.co.uk>
2017-10-09 14:56:57 +0100

    Refactor threshold logic

--- lib/alarm.rb
+++ lib/alarm.rb
@@ -5,27 +5,18 @@ class Alarm
   attr_reader :is_alarm_on
 
   def initialize
-    @low_pressure_threshold = 17
-    @high_pressure_threshold = 21
+    @threshold = (17..21)
     @sensor = Sensor.new()
     @is_alarm_on = false
   end
 
   def check
-    if pressure_too_low or pressure_too_high
+    unless @threshold.include?(psi_pressure_value)
       @is_alarm_on = true
     end
   end
 
   private
-
-  def pressure_too_low
-    psi_pressure_value < @low_pressure_threshold
-  end
-
-  def pressure_too_high
-    psi_pressure_value > @high_pressure_threshold
-  end
 
   def psi_pressure_value
     @sensor.pop_next_pressure_psi_value
