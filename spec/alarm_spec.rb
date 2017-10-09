7146c334240ec8e9e40d678430bd1bc8d6c3fd66
Dan Brooks <dmbrooks@live.co.uk>
2017-10-09 14:16:16 +0100

    Add test for when alarm should be on

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Alarm do
   it 'should be turned off by default' do
     expect(subject.is_alarm_on).to be(false)
   end
+
+  it 'should turn on when pressure is not within threshold range' do
+    subject.check
+    expect(subject.is_alarm_on).to be(true)
+  end
+
 end
